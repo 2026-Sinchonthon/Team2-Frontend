14fd0163043998760a64f58f61c7a8c331bc502b
문지호 <moonjiho0706@yonsei.ac.kr>
2026-08-29 18:52:00 +0900

    fix: 마이페이지 디자인 수정

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
 import iconBack from "../assets/icons/back.svg";
+import iconHeartSolid from "../assets/icons/heart-solid.svg";
+import iconProfileDefault from "../assets/icons/profile-default.svg";
+import iconSettings from "../assets/icons/settings.svg";
 
 function MyPage() {
   const notifyNotImplemented = () => {
@@ -17,7 +20,9 @@ function MyPage() {
         onClick={notifyNotImplemented}
         className="absolute left-[23px] top-[133px] flex h-[97px] w-[346px] items-center gap-4 rounded-[10px] border border-[#d2d2d1] px-5"
       >
-        <div className="size-[66px] shrink-0 rounded-full bg-gray-200" />
+        <div className="flex size-[66px] shrink-0 items-center justify-center rounded-full border border-[#f74651] bg-white">
+          <img src={iconProfileDefault} alt="" className="size-6" />
+        </div>
         <div className="text-left">
           <p className="text-[18px] font-bold text-[#1f1c1a]">아기사자</p>
           <p className="mt-1 text-xs text-[#bcbbba]">이화여자대학교 인증</p>
@@ -29,7 +34,7 @@ function MyPage() {
           to="/mypage/activity"
           className="flex h-[66px] w-full items-center gap-3 rounded-[10px] border border-[#d2d2d1] px-4"
         >
-          <div className="size-9 shrink-0 rounded-full bg-gray-200" />
+          <img src={iconHeartSolid} alt="" className="size-9 shrink-0 p-1.5" />
           <span className="flex-1 text-[16px] font-bold text-[#1f1c1a]">
             내 활동
           </span>
@@ -41,7 +46,7 @@ function MyPage() {
           onClick={notifyNotImplemented}
           className="mt-4 flex h-[66px] w-full items-center gap-3 rounded-[10px] border border-[#d2d2d1] px-4"
         >
-          <div className="size-9 shrink-0 rounded-full bg-gray-200" />
+          <img src={iconSettings} alt="" className="size-9 shrink-0 p-1.5" />
           <span className="flex-1 text-left text-[16px] font-bold text-[#1f1c1a]">
             설정
           </span>
