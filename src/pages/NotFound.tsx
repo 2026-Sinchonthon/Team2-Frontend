b1bee9b70426f5e67d553eae8263e8757888fb31
문지호 <moonjiho0706@yonsei.ac.kr>
2026-08-29 10:11:28 +0900

    feat: initial setting

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <section style={{ padding: '4rem 2rem', textAlign: 'center' }}>
+      <h1>404</h1>
+      <p>페이지를 찾을 수 없습니다.</p>
+    </section>
+  )
+}
+
+export default NotFound
